3476f4a2c008d4ee367f650568e99a49a1af2ae8
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-10-25 14:57:53 +0200

    HTML structure and styling adjusted: game area and platforms well integrated

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 class Game {
+    static border = "3px solid rgb(110, 110, 110)";
+    static borderWidth = parseInt(Game.border, 10);
     constructor () {
         this.lives = 3;
         this.element = document.querySelector('#game-area');
         this.width = this.element.getBoundingClientRect().width;
         this.height = this.element.getBoundingClientRect().height;
+        this.element.style.border = Game.border;
     }
 }
 
@@ -11,6 +14,8 @@ const myGame = new Game();
 
 class Platform {
     static height = 30;
+    static border = "2px solid black";
+    static borderWidth = parseInt(Platform.border, 10);
     constructor (x, y) { 
     /* initial position */
     this.x = x;
@@ -27,24 +32,25 @@ class Platform {
     this.element.style.top = this.y+ "px";
     /* this.element.style.width = (this.width) + "px"; */
     this.element.style.height = (Platform.height) + "px";
-    const styleHeight = this.element.style.height;
-    console.log("this.element.style.height ", styleHeight);
+    this.element.style.border = Platform.border;
 
 
     myGame.element.appendChild(this.element);
     }
 }
-const gap = (myGame.height - Platform.height)/5.5;
+
+
+const gap = (myGame.height - Game.borderWidth - 4*Platform.height)/4;
+console.log ("myPlatform border width ", Platform.borderWidth)
 console.log("Platform.height ",Platform.height);
 console.log ("Platform border width ",Platform.borderWidth)
 console.log("gap ", gap);
 console.log("gamearea height ", myGame.height);
 console.log("gamearea border width ", Game.borderWidth);
-/* ARRIVATO QUI */
 
 const myPlatforms = [
-    new Platform(0, myGame.height - Platform.height - Platform.borderWidth), 
-    new Platform(0, myGame.height - 2 * Platform.height - gap),
-    new Platform(0, myGame.height - 3 * Platform.height - 2 * gap),
-    new Platform(0, myGame.height - 4 * Platform.height - 3*gap)
+    new Platform(0, myGame.height - Game.borderWidth - Platform.height - Platform.borderWidth), 
+    new Platform(0, myGame.height - Game.borderWidth - 2 * Platform.height - Platform.borderWidth - gap),
+    new Platform(0, myGame.height - 3 * Platform.height - Platform.borderWidth - 2 * gap),
+    new Platform(0, myGame.height - 4 * Platform.height - Platform.borderWidth - 3*gap)
 ]
