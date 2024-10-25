e41379922fb1babfc69aec44aca43ddbdccc3409
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-10-25 17:36:01 +0200

    logic.js added to previous commit

---
+++ logic.js
@@ -0,0 +1,28 @@
+document.addEventListener("keydown", (press) => {
+    switch (press.key) {
+        case "ArrowLeft":
+            myPlayer.direction = "left";
+            break;
+        case "ArrowRight":
+            myPlayer.direction = "right";
+            break;
+        default:
+            break;
+    }
+})
+
+document.addEventListener("keyup", (unpress) => {
+    if (unpress.key === "ArrowLeft" || unpress.key === "ArrowRight") {
+        myPlayer.direction = null;
+    }
+});
+
+let frames = 0;
+
+function gameLoop () {
+    requestAnimationFrame(gameLoop);
+    frames++;
+    myPlayer.walk(myPlayer.direction);
+}
+
+requestAnimationFrame(gameLoop);
