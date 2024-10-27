ebebf6d961255dcee6241ecd658b687f17f15005
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-10-27 18:18:23 +0100

    formatted

--- logic.js
+++ logic.js
@@ -1,32 +1,31 @@
 document.addEventListener("keydown", (press) => {
-    switch (press.key) {
-        case "ArrowLeft":
-            myPlayer.direction = "left";
-            break;
-        case "ArrowRight":
-            myPlayer.direction = "right";
-            break;
-        case " ":
-        myPlayer.jump();
-        break;
-        default:
-            break;
-    }
-})
-
-document.addEventListener("keyup", (unpress) => {
-    if (unpress.key === "ArrowLeft" || unpress.key === "ArrowRight") {
-        myPlayer.direction = null;
-    }
+  switch (press.key) {
+    case "ArrowLeft":
+      myPlayer.direction = "left";
+      break;
+    case "ArrowRight":
+      myPlayer.direction = "right";
+      break;
+    case " ":
+      myPlayer.jump();
+      break;
+    default:
+      break;
+  }
 });
 
+document.addEventListener("keyup", (unpress) => {
+  if (unpress.key === "ArrowLeft" || unpress.key === "ArrowRight") {
+    myPlayer.direction = null;
+  }
+});
 
 let frames = 0;
 
-function gameLoop () {
-    requestAnimationFrame(gameLoop);
-    frames++;
-    myPlayer.walk(myPlayer.direction);
+function gameLoop() {
+  requestAnimationFrame(gameLoop);
+  frames++;
+  myPlayer.walk(myPlayer.direction);
 }
 
 requestAnimationFrame(gameLoop);
--- player.js
+++ player.js
@@ -1,80 +1,86 @@
 class Player {
-    static height = 50; //this is only a value, but does not create visually the player. For that, I need to use this value with this.element.styel.height.
-    static width = 30;
-    static border = "1px solid black";
-    static borderWidth = parseInt(Player.border, 10);
+  static height = 50; //this is only a value, but does not create visually the player. For that, I need to use this value with this.element.styel.height.
+  static width = 30;
+  static border = "1px solid black";
+  static borderWidth = parseInt(Player.border, 10);
 
-    constructor () {
-        //visual
-        this.element = document.createElement("div");
-        this.element.setAttribute("id", "player");
-        myGame.element.appendChild(this.element);
+  constructor() {
+    //visual
+    this.element = document.createElement("div");
+    this.element.setAttribute("id", "player");
+    myGame.element.appendChild(this.element);
 
-        this.element.style.height = (Player.height) + "px";
-        this.element.style.width = (Player.width) + "px";
-        this.element.style.border = Player.border;
+    this.element.style.height = Player.height + "px";
+    this.element.style.width = Player.width + "px";
+    this.element.style.border = Player.border;
 
-        // initial position on the first platform
-        this.element.style.left = 10 + "px";
-        this.x = parseInt(this.element.style.left);
-        /* this.y = firstPlatform.y; */
-        this.element.style.top = (firstPlatform.y - Player.height) + 'px'
-        this.y = parseInt(this.element.style.top);
+    // initial position on the first platform
+    this.element.style.left = 10 + "px";
+    this.x = parseInt(this.element.style.left);
+    /* this.y = firstPlatform.y; */
+    this.element.style.top = firstPlatform.y - Player.height + "px";
+    this.y = parseInt(this.element.style.top);
 
-        //features (speed, etc.)
-        this.speed = 2;
-        this.direction = null;
+    //features (speed, etc.)
+    this.speed = 2;
+    this.direction = null;
 
-        //jump features
-        this.jumpSpeed = 5; //prima 3
-        this.currentSpeed = this.jumpSpeed;
-        this.gravity = 0.08;
+    //jump features
+    this.jumpSpeed = 5; //prima 3
+    this.currentSpeed = this.jumpSpeed;
+    this.gravity = 0.08;
+    this.jumping = false;
+  }
+
+  walk(direction) {
+    if (direction === "left") {
+      this.x -= this.speed;
+      if (this.x <= 0) {
+        this.x = 0;
+      }
+    } else if (direction === "right") {
+      this.x += this.speed;
+      if (
+        this.x >=
+        myGame.width -
+          2 * Game.borderWidth -
+          Player.width -
+          2 * Player.borderWidth
+      ) {
+        /* if (this.x >= myGame.width - Game.borderWidth - Player.width - Player.borderWidth) { */
+        // this.x = myGame.width - 2*Game.borderWidth - Player.width;
+        /* this.x = myGame.width - Game.borderWidth - Player.width - Player.borderWidth */
+        this.x = myGame.width - 2 * Game.borderWidth - Player.width;
+      }
+    }
+    this.element.style.left = this.x + "px";
+
+    // check if the player is jumping, the vertical position y is updated
+    if (this.jumping) {
+      this.y -= this.currentSpeed;
+      this.currentSpeed -= this.gravity;
+
+      /*    this.checkPlatformCollision(); */
+
+      if (this.y >= firstPlatform.y - Player.height) {
+        //the player lands again on the upper side of the platform
         this.jumping = false;
-        
+        this.currentSpeed = this.jumpSpeed;
+        this.y = firstPlatform.y - Player.height;
+      }
+
+      this.element.style.top = this.y + "px";
     }
+  }
 
-    walk(direction) {
-        if (direction === "left") {
-            this.x -= this.speed;
-            if (this.x <=0) {
-                this.x =0;
-            }
-        } else if (direction === "right") {
-            this.x += this.speed;
-            if (this.x >= myGame.width - 2*Game.borderWidth - Player.width - 2*Player.borderWidth) {
-            /* if (this.x >= myGame.width - Game.borderWidth - Player.width - Player.borderWidth) { */
-                // this.x = myGame.width - 2*Game.borderWidth - Player.width;
-                /* this.x = myGame.width - Game.borderWidth - Player.width - Player.borderWidth */
-                this.x = myGame.width - 2*Game.borderWidth - Player.width;
-            }
-        }
-        this.element.style.left = this.x + 'px';
-
-        // check if the player is jumping, the vertical position y is updated
-        if (this.jumping) {
-            this.y -= this.currentSpeed;
-            this.currentSpeed -= this.gravity;
-
-        /*    this.checkPlatformCollision(); */
-
-            if (this.y >= firstPlatform.y - Player.height) { //the player lands again on the upper side of the platform
-                this.jumping = false; 
-                this.currentSpeed = this.jumpSpeed;
-                this.y = firstPlatform.y - Player.height;
-            }
-
-            this.element.style.top = this.y + "px";
-        } 
+  jump() {
+    if (!this.jumping) {
+      this.jumping = true;
     }
-    
-    jump() {
-            if(!this.jumping) {
-                this.jumping = true; 
-            }
-        }
+  }
 
-        //uncomment also line 54
-/*     checkPlatformCollision() {
+  //uncomment also line 54
+  /*     checkPlatformCollision() {
         myPlatforms.forEach(platform => {
             if (this.y <= platform.y + platform.height && this.y > platform.y) {
             this.jumping = false;
@@ -90,7 +96,7 @@ class Player {
             }
             }); 
         } */
-    }
+}
 
 const myPlayer = new Player();
 console.log("myPlayer x ", myPlayer.x);
